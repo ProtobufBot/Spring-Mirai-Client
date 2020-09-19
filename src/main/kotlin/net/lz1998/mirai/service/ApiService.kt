80d94e683705967c1c6bcc773ccf0da8e43c0216
lizheng.lz1998 <lizheng.lz1998@bytedance.com>
2020-09-20 02:25:24 +0800

    event api service

---
+++ src/main/kotlin/net/lz1998/mirai/service/ApiService.kt
@@ -0,0 +1,9 @@
+package net.lz1998.mirai.service
+
+import org.springframework.stereotype.Service
+
+// 处理 server 发过来的API，执行并返回结果
+@Service
+class ApiService {
+
+}
---
+++ src/main/kotlin/net/lz1998/mirai/service/EventService.kt
@@ -0,0 +1,9 @@
+package net.lz1998.mirai.service
+
+import org.springframework.stereotype.Service
+
+// 处理 Bot 收到的 Event，发送给server
+@Service
+class EventService {
+
+}
