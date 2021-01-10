ce86718a125a890fcc2b615d0c5fa85976dcf3a5
lizheng.lz1998 <lizheng.lz1998@bytedance.com>
2021-01-10 17:07:30 +0800

    auto create

--- src/main/kotlin/net/lz1998/mirai/properties/ClientProperties.kt
+++ src/main/kotlin/net/lz1998/mirai/properties/ClientProperties.kt
@@ -1,9 +1,13 @@
 package net.lz1998.mirai.properties
 
+import org.springframework.beans.factory.annotation.Value
 import org.springframework.boot.context.properties.ConfigurationProperties
+import org.springframework.stereotype.Component
 
 
+@Component
 @ConfigurationProperties("bot.client")
-class ClientProperties {
+object ClientProperties {
+    @Value("\${bot.client.wsUrl}")
     var wsUrl = "ws://127.0.0.1/ws/cq/"
 }
--- src/main/kotlin/net/lz1998/mirai/service/BotService.kt
+++ src/main/kotlin/net/lz1998/mirai/service/BotService.kt
@@ -1,27 +1,37 @@
 package net.lz1998.mirai.service
 
 import dto.HttpDto
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.launch
+import kotlinx.serialization.Serializable
+import kotlinx.serialization.json.Json
 import net.lz1998.mirai.entity.WebsocketBotClient
+import net.lz1998.mirai.entity.json
 import net.lz1998.mirai.properties.ClientProperties
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.stereotype.Service
+import java.io.File
 
 @Service
 class BotService {
+    init {
+        autoCreate("bots.json")
+    }
+
     val botMap = mutableMapOf<Long, WebsocketBotClient>()
 
     @Autowired
     lateinit var clientProperties: ClientProperties
 
     @Synchronized
-    suspend fun createBot(botId: Long, password: String) {
+    suspend fun createBot(botId: Long, password: String, wsUrl: String = clientProperties.wsUrl) {
         var bot = botMap[botId]
         // 如果有旧的，关掉旧的
         bot?.bot?.close()
         bot?.wsClient?.close(1001, "")
 
         // 开新的
-        bot = WebsocketBotClient(botId, password, wsUrl = clientProperties.wsUrl)
+        bot = WebsocketBotClient(botId, password, wsUrl = wsUrl)
         botMap[botId] = bot
         bot.initBot()
     }
@@ -42,5 +52,34 @@ class BotService {
         val bot = botMap[botId]
         bot?.bot?.login()
     }
+
+    // 自动使用配置文件创建
+    private final fun autoCreate(filepath: String) {
+        File(filepath).loadAsBotInfo(json).accounts.forEach {
+            GlobalScope.launch {
+                println("auto create ${it.uin}")
+                createBot(it.uin, it.password, it.wsUrl)
+            }
+        }
+    }
 }
 
+
+fun File.loadAsBotInfo(json: Json): BotInfo {
+    if (!this.exists() || this.length() == 0L) {
+        return BotInfo(listOf())
+    }
+    return json.decodeFromString(BotInfo.serializer(), this.readText())
+}
+
+@Serializable
+class BotInfo(
+        val accounts: List<BotAccount>
+)
+
+@Serializable
+class BotAccount(
+        val uin: Long,
+        val password: String,
+        val wsUrl: String,
+)
